7c0b6b5d23fcadf03eda2d930b21d7596943edb9
iii-source <quercus1109@gmail.com>
2020-08-05 15:53:13 +0900

    update /user/login パス変更

--- app/server_login.py
+++ app/server_login.py
@@ -1,20 +1,24 @@
 import os
 from flask import Flask, request
-from .information_gathering import tweet_main
+# from .information_gathering import tweet_main
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/user/login', methods=["GET"])
 def login():
     if request.get_json()['API_KEY'] == os.environ.get('API_KEY'):
         # # 認証OKの場合
-        return 200
+        return '200'
     else:
         # 認証NGの場合
-        return 401
-    
-    tweet_main()
+        return '401'
+
+
+# # TODO メイン処理呼び出し
+# @app.route('/tweet_main', methods=["GET", "POST"])
+# def main():
+#     tweet_main()
 
 
 if __name__ == '__main__':
