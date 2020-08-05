920bbe01528ceec76168a08cf8bfc8aca609bd11
iii-source <quercus1109@gmail.com>
2020-08-05 15:54:30 +0900

    update get_login_url関数追加

--- tests/test_server_login.py
+++ tests/test_server_login.py
@@ -1,15 +1,12 @@
 import os
 import requests
 
-# テスト対象のURLを定義
-url = 'http://localhost:5000/login'
-
 
 # 認証OKの場合
 def test_server_login01():
     # リクエストAPI用jsonデータ作成
     payload = {'API_KEY': os.environ.get('API_KEY')}
-    result = get_request(payload)
+    result = get_request(get_login_url(), payload)
     assert result == 200
 
 
@@ -17,16 +14,23 @@ def test_server_login01():
 def test_server_login02():
     # リクエストAPI用jsonデータ作成
     payload = {'API_KEY': 'value_for_NG'}
-    result = get_request(payload)
+    result = get_request(get_login_url(), payload)
     assert result == 401
 
 
-def get_request(payload):
+def get_login_url():
+    # テスト対象のURLを定義
+    return 'http://localhost:5000/user/login'
+
+
+def get_request(url, payload):
     """
     getAPI用リクエスト
 
     Parameters
     ----------
+    url : string
+        リクエスト用url
     payload : dict
         リクエスト用データ
 
