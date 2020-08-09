a9d73deee6167a302407e72f346a72775528b5a0
iii-source <quercus1109@gmail.com>
2020-08-09 16:05:56 +0900

    update サーバーログインレスポンス json形式対応

--- tests/test_server_login.py
+++ tests/test_server_login.py
@@ -7,7 +7,8 @@ def test_server_login01():
     # リクエストAPI用jsonデータ作成
     payload = {'API_KEY': os.environ.get('API_KEY')}
     result = get_request(get_login_url(), payload)
-    assert result == 200
+    assert result['message'] == 'accepted the certificate.'
+    assert result['code'] == 200
 
 
 # 認証NGの場合
@@ -15,7 +16,8 @@ def test_server_login02():
     # リクエストAPI用jsonデータ作成
     payload = {'API_KEY': 'value_for_NG'}
     result = get_request(get_login_url(), payload)
-    assert result == 401
+    assert result['message'] == 'certification failed.'
+    assert result['code'] == 401
 
 
 def get_login_url():
