0dd2e7be2f21e1af1194f8b8926cebd20dc7da6b
iii-source <quercus1109@gmail.com>
2020-08-09 16:12:44 +0900

    update ロギング処理定数 sql.yaml読み込み処理追加

--- common/db/__init__.py
+++ common/db/__init__.py
@@ -1,1 +1,8 @@
+import yaml
+
 LOGGING_PATH = '../common/log/logger.log'
+LOGGING_PATH_CURRENT = '../log/logger.log'
+API_DOCUMENTS = 'https://iii-source.github.io/public/swagger_ui/tweet_news/docs/dist/'
+
+with open('../common/db/sql/sql.yaml', 'r') as read_yml:
+    sql_yaml = yaml.load(read_yml, Loader=yaml.SafeLoader)
