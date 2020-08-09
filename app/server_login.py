76004d5c4d15ebe835dadf04a48ac188feccd215
iii-source <quercus1109@gmail.com>
2020-08-09 16:10:42 +0900

    update レスポンスjson形式対応 DB接続インスタンス化

--- app/server_login.py
+++ app/server_login.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request
 # from .information_gathering import tweet_main
-from common.db import postgre_sample2 as postgre
+from common.db import postgre_sample2 as postgres
+from common.response_message import response, error_response
 
 app = Flask(__name__)
 
@@ -10,15 +11,15 @@ app = Flask(__name__)
 def login():
     if request.get_json()['API_KEY'] == os.environ.get('API_KEY'):
         # # 認証OKの場合
-        return '200'
+        return response.response_200()
     else:
         # 認証NGの場合
-        return '401'
+        return response.response_401()
 
 
 @app.route('/news/<newsid>', methods=["GET"])
 def news(newsid):
-    return postgre.select(connector, cursor, newsid)
+    return postgres_instance.select(newsid)
 
 
 # # TODO メイン処理呼び出し
@@ -28,6 +29,6 @@ def news(newsid):
 
 
 if __name__ == '__main__':
-    connector, cursor = postgre.connect_db()
+    postgres_instance = postgres.PostgresSample2()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
