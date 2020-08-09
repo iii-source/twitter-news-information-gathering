ecbd3fabac6fe8e58fa1a8f0aa159be5beaf0369
iii-source <quercus1109@gmail.com>
2020-08-09 16:23:18 +0900

    add response.py https レスポンスjson形式対応

---
+++ common/response_message/response.py
@@ -0,0 +1,22 @@
+from common.response_message import API_DOCUMENTS
+
+
+def response_200():
+    response_dict = {}
+    response_dict['message'] = 'accepted the certificate.'
+    response_dict['code'] = 200
+    return response_dict
+
+
+def response_401():
+    response_dict = {}
+    response_dict['message'] = 'certification failed.'
+    response_dict['code'] = 401
+    return response_dict
+
+
+def response_404():
+    response_dict = {}
+    response_dict['message'] = 'not found newsid'
+    response_dict['code'] = 404
+    return response_dict
