aab5d7a1255d15699787fb8fa45ed129d1fa071c
iii-source <quercus1109@gmail.com>
2020-08-15 19:12:43 +0900

    add tests/test_put_news.py news更新機能テスト追加

---
+++ tests/test_put_news.py
@@ -0,0 +1,100 @@
+import requests
+
+URL_REF = 'https://iii-source.github.io/public/' \
+          'swagger_ui/tweet_news/docs/dist/'
+
+
+# 正常系 通常更新
+def test_news01():
+    # リクエストAPI用jsonデータ作成
+    payload = {'description': 'description_test'}
+    result = put_request(get_url('3'), payload)
+    assert result['message'] == 'Updated successfully.'
+    assert result['code'] == 200
+
+
+# 正常系 更新が正しく出来ているか確認
+def test_news02():
+    result = get_request(get_url('3'))
+    for record in result['records']:
+        assert record['newsid'] == 3
+        assert record['news_date'] == '2020-08-10'
+        assert record['url'] == 'https://hogehoge3'
+        assert record['description'] == 'description_test'
+
+
+# 正常系 検索にヒットしなかった場合
+def test_news03():
+    # リクエストAPI用jsonデータ作成
+    result = get_request(get_url('99'))
+    assert result['message'] == 'not found newsid'
+    assert result['code'] == 404
+
+
+# 異常系 不正なデータ型(数字型)
+def test_news04():
+    # リクエストAPI用jsonデータ作成
+    result = get_request(get_url('AAAAA'))
+    assert result['message'] == 'Bad Request'
+    assert result['errors']['code'] == 400
+    assert result['errors']['url_ref'] == URL_REF
+
+
+# 異常系 SQL インジェクション
+def test_news05():
+    # リクエストAPI用jsonデータ作成
+    result = get_request(get_url('10 or 1 = 1'))
+    assert result['message'] == 'Bad Request'
+    assert result['errors']['code'] == 400
+    assert result['errors']['url_ref'] == URL_REF
+
+    result = get_request(get_url('0; 1 = 1'))
+    assert result['message'] == 'Bad Request'
+    assert result['errors']['code'] == 400
+    assert result['errors']['url_ref'] == URL_REF
+
+    result = get_request(get_url('0; select * from news'))
+    assert result['message'] == 'Bad Request'
+    assert result['errors']['code'] == 400
+    assert result['errors']['url_ref'] == URL_REF
+
+
+def get_url(id):
+    # テスト対象のURLを定義
+    return 'http://localhost:5000/news/' + id
+
+
+def get_request(url):
+    """
+    getAPI用リクエスト
+
+    Parameters
+    ----------
+    url : string
+        リクエスト用url
+
+    Returns
+    -------
+    request_data : dict
+        jsonパースしたResponseデータ
+    """
+    return requests.get(url).json()
+
+
+def put_request(url, payload):
+    """
+    getAPI用リクエスト
+
+    Parameters
+    ----------
+    url : string
+        リクエスト用url
+    payload : dict
+        リクエスト用データ
+
+    Returns
+    -------
+    request_data : dict
+        jsonパースしたResponseデータ
+    """
+    return requests.put(url, json=payload).json()
