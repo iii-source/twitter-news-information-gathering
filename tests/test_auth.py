9b4cbbfd0d8ba3975688b60b29be2203315884e5
iii-source <quercus1109@gmail.com>
2020-10-04 21:26:34 +0900

    add tests/test_auth.py tests/tests_data_param/get_auth_param.py 認証用テスト追加

---
+++ tests/test_auth.py
@@ -0,0 +1,62 @@
+from tests import requests_operation as ro
+from tests.tests_data_param import get_auth_param as td
+import pytest
+
+URL_REF = 'https://iii-source.github.io/public/' \
+          'swagger_ui/tweet_news/docs/dist/'
+
+
+# 有効期限内用uuid取得
+@pytest.fixture()
+@pytest.mark.parametrize("inputs", list(td.get_test_auth().values()),
+                         ids=list(td.get_test_auth().keys()))
+def constant_uuid(inputs):
+    result = ro.get_request_with_auth(get_url(), inputs['user'], inputs['password'])
+    # tokenを取得
+    return max(record['token'] for record in result['records'])
+
+
+# 正常系 有効期限内の場合
+@pytest.mark.parametrize("inputs", list(td.get_test_auth().values()),
+                         ids=list(td.get_test_auth().keys()))
+def test_user_login01(inputs, constant_uuid):
+    # リクエストAPI用jsonデータ作成
+    result = ro.get_request_with_auth(get_url(), inputs['user'], inputs['password'])
+
+    # fixtureでは事前にuuidを払い出してもらう(固定)
+    # actual fixtureで事前に用意したuuid
+    # expect 今回新たに取得したuuid
+    # actual expect は同じになるはず
+    for record in result['records']:
+        assert record['token'] == constant_uuid
+
+
+# TODO 有効期限切れuuidを事前に取得し更新後uuidと比較できるようにする。
+# 正常系 有効期限切れの場合
+# @pytest.mark.parametrize("inputs", list(td.get_test_auth().values()),
+#                          ids=list(td.get_test_auth().keys()))
+# def test_user_login02(inputs, constant_uuid):
+#     # リクエストAPI用jsonデータ作成
+#     result = ro.get_request_with_auth(get_url(), inputs['user'], inputs['password'])
+#     # fixtureでは事前にuuidを払い出してもらう(固定)
+#     # actual fixtureで事前に用意したuuid
+#     # expect 今回新たに取得したuuid
+#     # actual expect は同じにならない uuidは更新されているので。
+#
+#     for record in result['records']:
+#         assert record['token'] != constant_uuid
+
+
+# 異常系 1: userが不一致, 2: passwordが不一致, 3: user passwordが不一致
+@pytest.mark.parametrize("inputs", list(td.get_test_auth03().values()),
+                         ids=list(td.get_test_auth03().keys()))
+def test_user_login03(inputs):
+    # リクエストAPI用jsonデータ作成
+    result = ro.get_request_with_auth(get_url(), inputs['user'], inputs['password'])
+    assert result.status_code == 401
+
+
+def get_url():
+    # テスト対象のURLを定義
+    return 'http://localhost:5000/user/login'
+
---
+++ tests/tests_data_param/get_auth_param.py
@@ -0,0 +1,31 @@
+"""
+テストケースに対するテスト名を定義
+"""
+test_key = "valid"
+test_key1 = "user_is_invalid"
+test_key2 = "password_is_invalid"
+test_key3 = "user_password_is_invalid"
+
+"""
+Basic認証用のuser passを取得する
+"""
+
+
+def get_test_auth():
+    test = {'user': 'john', 'password': 'hello'}
+    test_data = {
+        test_key: test,
+    }
+    return test_data
+
+
+def get_test_auth03():
+    test1 = {'user': 'foo', 'password': 'hello'}
+    test2 = {'user': 'john', 'password': 'foo'}
+    test3 = {'user': 'foo', 'password': 'foo'}
+    test_data = {
+        test_key1: test1,
+        test_key2: test2,
+        test_key3: test3,
+    }
+    return test_data
