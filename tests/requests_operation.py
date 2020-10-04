7810daceb17be9eb97274422b3eced57cfb1fa08
iii-source <quercus1109@gmail.com>
2020-10-04 21:27:56 +0900

    update tests/requests_operation.py 認証用request追加

--- tests/requests_operation.py
+++ tests/requests_operation.py
@@ -18,6 +18,33 @@ def get_request(url):
     return requests.get(url).json()
 
 
+def get_request_with_auth(url, user, password):
+    """
+    getAPI用リクエスト(Basic認証用)
+
+    Parameters
+    ----------
+    url : string
+        リクエスト用url
+    user : string
+        basic認証用user
+    password : string
+        basic認証用password
+
+    Returns
+    -------
+    result : dict
+        jsonパースした(or そのままの)Responseデータ
+    """
+    result = requests.get(url, auth=(user, password), verify=False)
+    if 'json' in result.headers.get('content-type'):
+        # application/jsonの場合jsonDecodeで返す
+        return result.json()
+    else:
+        # text/htmlの場合そのまま返す
+        return result
+
+
 def get_request_with_body(url, payload):
     """
     getAPI用リクエスト
